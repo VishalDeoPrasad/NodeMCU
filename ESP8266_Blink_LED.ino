a9558d444ffc3310163f0c6bcd0b5d52d2d5016d
VishalDeoPrasad <44454324+VishalDeoPrasad@users.noreply.github.com>
2022-12-17 20:28:46 +0530

    Add files via upload
    NodeMCU(ESP8266) Blink Led program

---
+++ ESP8266_Blink_LED.ino
@@ -0,0 +1,12 @@
+int ledPin=D8; 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // LED On
+  delay(500);             // delay (1000 = 1s)
+  digitalWrite(ledPin, LOW);  // LED Off
+  delay(500);             // delay
+
+}
