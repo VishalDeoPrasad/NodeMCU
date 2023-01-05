696d9c61beb193a9213b893d90944b4963c8b1ab
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-06 01:38:18 +0530

    Serial Monitor

---
+++ Tilt_Piezo_Sensor_Serial_Monitor.ino
@@ -0,0 +1,23 @@
+int piezo = D2;
+int tilt = D3;
+int piezo_val;
+int tilt_val;
+void setup() {
+  pinMode(piezo, INPUT);
+  pinMode(tilt, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  piezo_val = digitalRead(piezo);
+  tilt_val = digitalRead(tilt);
+  Serial.print("Piezo Value = ");
+  Serial.print(piezo_val);
+  Serial.print('\t');
+  Serial.print("Tilt Value = ");
+  Serial.println(tilt_val);
+  
+  
+
+}
